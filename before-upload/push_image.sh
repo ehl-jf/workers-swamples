20962e874f26752d9cefbb1e6dc74be5b1a9d8a7
Esteguet <hervee@jfrog.com>
2024-08-26 10:13:36 +0200

    Add after create

--- before-upload/push_image.sh
+++ before-upload/push_image.sh
@@ -3,8 +3,8 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 VERSION=${1:-1.0.0}
 
-JDP=localhost:8082
-IMAGE_TAG="${JDP}/docker-local/hello:${VERSION}"
+JPD=${JPD:-localhost:8082}
+IMAGE_TAG="${JPD}/docker-local/hello:${VERSION}"
 
 docker build -t ${IMAGE_TAG} -f ${DIR}/images/withProperties.dockerfile ${DIR}/images
 docker push ${IMAGE_TAG}
